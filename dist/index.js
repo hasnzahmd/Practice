28bfaf19007e7102a21f971b7a14db09d5d74c2e
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-01-27 23:57:37 +0500

    update

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
 let age = 20;
-console.log("hello", age);
+if (age < 30)
+    age += 10;
+console.log(age);
+let num = 123456789;
+let course = "typescript";
+let is_published = false;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-let age: number = 20;
+let age= 20;
     if(age < 30)
 age += 10;
 console.log(age);
+
+let num = 123_456_789;
+let course = "typescript";
+let is_published = false;
+
