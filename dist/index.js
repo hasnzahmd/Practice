8130d964613ea0827a815a1d8dce62f781c51a53
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-01-28 00:38:27 +0500

    compile

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,28 @@ console.log(age);
 let num = 123456789;
 let course = "typescript";
 let is_published = false;
+function render(document) {
+    console.log(document);
+}
+let numbers = [];
+numbers[0] = 1;
+numbers[1] = 2;
+console.log(numbers.forEach(n => n.toFixed));
+let user = [1, 'jhon'];
+var Size;
+(function (Size) {
+    Size[Size["small"] = 1] = "small";
+    Size[Size["medium"] = 2] = "medium";
+    Size[Size["large"] = 3] = "large";
+})(Size || (Size = {}));
+let size1 = Size.large;
+let size2 = "l";
+console.log(size1, size2);
+function calculate(tax, amount = 30000) {
+    if (amount < 50000)
+        return tax * 0.1;
+    else
+        return tax * 0.2;
+}
+calculate(20);
 //# sourceMappingURL=index.js.map
