3ddea5e72f7265b845a51e3fd5aac9ee96ce60e1
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-02-06 23:58:28 +0500

    practiced tailwind css

--- Next.js/app/users/page.tsx
+++ Next.js/app/users/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 interface User {
@@ -12,11 +13,14 @@ const UsersPage = async () => {
 
   return (
     <>
-        <h1>Users</h1>
-        <p>{new Date().toLocaleTimeString()}</p>
-        <ul>
-            {users.map((user)=> <li key={user.id}>{user.name}</li>)}
+        <h1 className='bg-green-400 text-center text-3xl text-white p-4'>Users Page</h1>
+        <p className='bg-gray-300 p-2 shadow-md shadow-black hover:shadow-none text-center border border-transparent hover:bg-gray-400 hover:border-4 hover:border-blue-400 duration-300'>{new Date().toLocaleTimeString()}</p>
+        <ul className=' flex flex-col gap-4 p-10 bg-blue-100 items-center justify-center'>
+            {users.map((user)=> <li className='bg-blue-200 w-2/4 py-1 text-center hover:bg-purple-300 hover:border-red-300 hover:border-2' key={user.id}>{user.name}</li>)}
         </ul>
+        <button className=' text-center bg-rose-600 mr-20 text-white w-screen p-2 border-0 hover:border-green-600 hover:border-2 hover:bg-rose-500 duration-200' >
+          <Link href='/'>Home</Link>
+        </button>
     </>
   )
 }
