f5de20c6af1ffffaabe8613ef13148a4dc931297
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-02-22 22:58:20 +0500

    variable and types

---
+++ Flutter/peactice.dart
@@ -0,0 +1,9 @@
+void main() {
+  //Variables and Types
+  var name = 'John Doe'; // Automatically inferred to be String
+  int age = 30; // Explicitly typed as integer
+  double height = 5.9; // Double for floating-point numbers
+  bool isRegistered = true; // Boolean values
+  print('$name is $age years old, $height tall, and registered: $isRegistered');
+
+}
